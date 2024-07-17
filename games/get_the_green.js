c8781bb9da89f7f511300ab9f6fb105bb0a0f330
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2024-07-17 18:44:39 +1000

    added purple level

--- games/get_the_green.js
+++ games/get_the_green.js
@@ -193,7 +193,18 @@ ggggggggggggg
 ggggggggggggg
 ggggggggggggg
 ggggggggggggg
-.............`
+.............`,
+  map`
+ggggggggggggg
+ggggggggggggg
+ggggggggggggg
+ggggggggggggg
+ggggggggggggg
+ggggggggggggg
+ggggggggggggg
+ggggggggggggg
+ggggggggggggg
+.............`,
 ]
 
 setMap(levels[level])
@@ -292,6 +303,8 @@ if(level == 0){
   checkGreen()
 } else if(level == 2){
   checkBlue()
+}else if(level == 3){
+  checkPurple()
 }
 }
 
@@ -341,6 +354,28 @@ if(level == 0){
     color: color`.`
   })
   }
+
+function level3(){
+    clearText()
+    addSprite(5, 4, player);
+
+      const randomX = Math.floor(Math.random() * 13);
+  const randomY = Math.floor(Math.random() * 9);
+  addSprite(randomX, randomY, red)
+  const randomX1 = Math.floor(Math.random() * 13);
+  const randomY1 = Math.floor(Math.random() * 9);
+  addSprite(randomX1, randomY1, red)
+  const randomX3 = Math.floor(Math.random() * 13);
+  const randomY3 = Math.floor(Math.random() * 9);
+  clearTile(randomX3, randomY3)
+  addSprite(randomX3, randomY3, grey)
+  addSprite(randomX3, randomY3, purple)
+  addText("Level " + ingameLevel, {
+    x: 1,
+    y: 14,
+    color: color`.`
+  })
+}
   
 
   function checkSelection() {
@@ -361,6 +396,15 @@ if(level == 0){
         level2()
       }
     }
+        const purpleSprites = getAll(purple)
+    for (const purpleSprite of purpleSprites) {
+      if (playerSprite.x === purpleSprite.x && playerSprite.y === purpleSprite.y) {
+        level = 3;
+        setMap(levels[level])
+        level3()
+      }
+    }
+    
   }
 
   function checkGreen() {
@@ -471,13 +515,76 @@ if(level == 0){
     }
   }
 
-  function startTimer() {
-    timer = 10;
+  function checkPurple() {
+    const playerSprite = getFirst(player);
+    const purpleSprite = getFirst(purple);
+    const redSprites = getAll(red)
+    if (playerSprite.x === purpleSprite.x && playerSprite.y === purpleSprite.y && gameOver === false) {
+      resetTimer()
+      purpleSprite.remove()
+      for (const redSprite of redSprites) {
+        redSprite.remove()
+      }
+      ingameLevel += 1
+      for (let i = 0; i < ingameLevel * 2; i++) {
+        const randomX = Math.floor((Math.random() * 13));
+        const randomY = Math.floor(Math.random() * 9);
+        addSprite(randomX, randomY, red)
+      }
+
+      const randomX = Math.floor((Math.random() * 13));
+      const randomY = Math.floor(Math.random() * 9);
+      clearTile(randomX, randomY)
+      addSprite(randomX, randomY, grey)
+      addSprite(randomX, randomY, purple)
+
+      if (playerSprite.x === randomX && playerSprite.y === randomY) {
+        getFirst(player).x = 5
+        getFirst(player).y = 4
+      }
+      const playerSprite1 = getFirst(player)
+      clearTile(playerSprite1.x, playerSprite1.y)
+      addSprite(playerSprite1.x, playerSprite1.y, grey)
+      addSprite(playerSprite1.x, playerSprite1.y, player)
+      addText("Level " + ingameLevel, {
+        x: 1,
+        y: 14,
+        color: color`.`
+      })
+      startTimer(5)
+
+
+    }
+    for (const redSprite of redSprites) {
+      if (playerSprite.x === redSprite.x && playerSprite.y === redSprite.y && gameOver === false) {
+        resetTimer(5)
+        clearText()
+        addText("Game Over! Lvl " + ingameLevel, {
+          x: 1,
+          y: 14,
+          color: color`.`
+        })
+        gameOver = true;
+      }
+    }
+  }
+
+
+  function startTimer(s) {
+    timer = s;
+    if(timer > 9){
     addText(timer.toString(), {
       x: 17,
       y: 14,
       color: color`.`
     })
+    } else{
+      addText("0" + timer.toString(), {
+        x: 17,
+        y: 14,
+        color: color`.`
+      })
+    }
     countdownInterval = setInterval(() => {
       timer--;
       addText("0" + timer.toString(), {
@@ -498,7 +605,7 @@ if(level == 0){
     }, 1000);
   }
 
-  function resetTimer() {
+  function resetTimer(s) {
     clearInterval(countdownInterval);
-    timer = 10;
+    timer = s;
   }
